bc1fe1c531313c712d9263b4f5a34e5de9039182
41343150 <41343150@nfu.edu.tw>
2025-11-18 00:25:58 +0800

    Update mywidget.cpp

--- mywidget.cpp
+++ mywidget.cpp
@@ -1,48 +1,49 @@
 #include "mywidget.h"
 #include "ui_mywidget.h"
+
 #include <QFile>
 #include <QTextStream>
 #include <QFileDialog>
 #include <QTableWidgetItem>
 #include <QDebug>
 
-QString mFilename = "C:/Users/user/Desktop/1117/file.txt";
+QString defaultFilename = "C:/Users/user/Desktop/1117/file.txt";
 
-void Write(QString Filename, QString str) {
-    QFile mFile(Filename);
-    if (!mFile.open(QFile::WriteOnly | QFile::Text)) {
+void WriteToFile(const QString &filename, const QString &content) {
+    QFile file(filename);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
         qDebug() << "could not open file for write";
         return;
     }
-
-    QTextStream out(&mFile);
-    out << str;
-    mFile.flush();
-    mFile.close();
+    QTextStream out(&file);
+    out << content;
+    file.close();
 }
 
-QString Read(QString Filename) {
-    QFile mFile(Filename);
-
-    if (!mFile.open(QFile::ReadOnly | QFile::Text)) {
+QString ReadFromFile(const QString &filename) {
+    QFile file(filename);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
         qDebug() << "could not open file for read";
         return "";
     }
-
-    QTextStream in(&mFile);
+    QTextStream in(&file);
     QString text = in.readAll();
-    mFile.close();
+    file.close();
     return text;
 }
 
 myWidget::myWidget(QWidget *parent)
-    : QWidget(parent), ui(new Ui::myWidget), currentFilename("") {
+    : QWidget(parent),
+    ui(new Ui::myWidget),
+    currentFilename("")
+{
     ui->setupUi(this);
 
-    QStringList ColTotle;
+    QStringList headers;
+    headers << "學號" << "班級" << "姓名" << "電話";
+
     ui->tableWidget->setColumnCount(4);
-    ColTotle << QStringLiteral("學號") << QStringLiteral("班級") << QStringLiteral("姓名") << QStringLiteral("電話");
-    ui->tableWidget->setHorizontalHeaderLabels(ColTotle);
+    ui->tableWidget->setHorizontalHeaderLabels(headers);
 }
 
 myWidget::~myWidget() {
@@ -50,18 +51,13 @@ myWidget::~myWidget() {
 }
 
 void myWidget::on_pushButton_2_clicked() {
-    QTableWidgetItem *inputRow1,*inputRow2,*inputRow3,*inputRow4;
-    inputRow1 = new QTableWidgetItem(QString(ui->lineEdit->text()));
-    inputRow2 = new QTableWidgetItem(QString(ui->lineEdit_2->text()));
-    inputRow3 = new QTableWidgetItem(QString(ui->lineEdit_3->text()));
-    inputRow4 = new QTableWidgetItem(QString(ui->lineEdit_4->text()));
+    int row = ui->tableWidget->rowCount();
+    ui->tableWidget->insertRow(row);
 
-    ui->tableWidget->insertRow(ui->tableWidget->rowCount());
-
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,0,inputRow1);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,1,inputRow2);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,2,inputRow3);
-    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,3,inputRow4);
+    ui->tableWidget->setItem(row, 0, new QTableWidgetItem(ui->lineEdit->text()));
+    ui->tableWidget->setItem(row, 1, new QTableWidgetItem(ui->lineEdit_2->text()));
+    ui->tableWidget->setItem(row, 2, new QTableWidgetItem(ui->lineEdit_3->text()));
+    ui->tableWidget->setItem(row, 3, new QTableWidgetItem(ui->lineEdit_4->text()));
 
     ui->lineEdit->clear();
     ui->lineEdit_2->clear();
@@ -75,28 +71,29 @@ void myWidget::on_pushButton_clicked() {
     if (!currentFilename.isEmpty()) {
         filename = currentFilename;
     } else {
-        filename = QFileDialog::getSaveFileName(this,
-                                                QStringLiteral("另存新檔"),
-                                                mFilename,
-                                                "Text Files (*.txt);;All Files (*)");
+        filename = QFileDialog::getSaveFileName(
+            this,
+            "另存新檔",
+            defaultFilename,
+            "Text Files (*.txt);;All Files (*)"
+            );
 
-        if (filename.isEmpty()) {
+        if (filename.isEmpty())
             return;
-        }
 
         currentFilename = filename;
     }
 
-    QString saveFile = "";
+    QString content;
     for (int i = 0; i < ui->tableWidget->rowCount(); i++) {
         for (int j = 0; j < ui->tableWidget->columnCount(); j++) {
-            saveFile += (j == 0) ? "" : ",";
-            saveFile += ui->tableWidget->item(i, j)->text();
+            content += (j == 0) ? "" : ",";
+            content += ui->tableWidget->item(i, j)->text();
         }
-        saveFile += "\n";
+        content += "\n";
     }
 
-    Write(filename, saveFile);
+    WriteToFile(filename, content);
 }
 
 void myWidget::on_pushButton_4_clicked() {
@@ -105,46 +102,46 @@ void myWidget::on_pushButton_4_clicked() {
     if (!currentFilename.isEmpty()) {
         filename = currentFilename;
     } else {
-        filename = QFileDialog::getSaveFileName(this,
-                                                QStringLiteral("另存新檔"),
-                                                mFilename,
-                                                "Text Files (*.txt);;All Files (*)");
+        filename = QFileDialog::getSaveFileName(
+            this,
+            "另存新檔",
+            defaultFilename,
+            "Text Files (*.txt);;All Files (*)"
+            );
 
-        if (filename.isEmpty()) {
+        if (filename.isEmpty())
             return;
-        }
 
         currentFilename = filename;
     }
 
-    QString saveFile = "";
+    QString content;
     for (int i = 0; i < ui->tableWidget->rowCount(); i++) {
         for (int j = 0; j < ui->tableWidget->columnCount(); j++) {
-            saveFile += (j == 0) ? "" : ",";
-            saveFile += ui->tableWidget->item(i, j)->text();
+            content += (j == 0) ? "" : ",";
+            content += ui->tableWidget->item(i, j)->text();
         }
-        saveFile += "\n";
+        content += "\n";
     }
 
-    Write(filename, saveFile);
+    WriteToFile(filename, content);
     close();
 }
 
 void myWidget::on_pushButton_3_clicked() {
-    QString filename = QFileDialog::getOpenFileName(this,
-                                                    QStringLiteral("選擇匯入的檔案"),
-                                                    mFilename,
-                                                    "Text Files (*.txt);;All Files (*)");
+    QString filename = QFileDialog::getOpenFileName(
+        this,
+        "選擇匯入的檔案",
+        defaultFilename,
+        "Text Files (*.txt);;All Files (*)"
+        );
 
-    if (filename.isEmpty()) {
+    if (filename.isEmpty())
         return;
-    }
 
-    QString text = Read(filename);
-    if (text.isEmpty()) {
-        qDebug() << "No data to import or file is empty";
+    QString text = ReadFromFile(filename);
+    if (text.isEmpty())
         return;
-    }
 
     currentFilename = filename;
     ui->tableWidget->setRowCount(0);
@@ -153,15 +150,11 @@ void myWidget::on_pushButton_3_clicked() {
 
     for (const QString &line : lines) {
         QStringList fields = line.split(",");
-
         if (fields.size() == 4) {
             int row = ui->tableWidget->rowCount();
             ui->tableWidget->insertRow(row);
-
-            for (int i = 0; i < 4; i++) {
-                QTableWidgetItem *item = new QTableWidgetItem(fields[i]);
-                ui->tableWidget->setItem(row, i, item);
-            }
+            for (int i = 0; i < 4; i++)
+                ui->tableWidget->setItem(row, i, new QTableWidgetItem(fields[i]));
         }
     }
 }
